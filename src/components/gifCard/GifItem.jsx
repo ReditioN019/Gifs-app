162288ce6898fb91015c91238034dbc83d129e79
Seba Rojo <srojoa19@gmail.com>
2022-07-20 17:26:46 -0400

    Se agrega copiar link de gif a portapapeles y compartirlo en whatssap

--- src/components/gifCard/GifItem.jsx
+++ src/components/gifCard/GifItem.jsx
@@ -1,27 +1,44 @@
-import { FaDownload } from 'react-icons/Fa';
 import { saveAs } from 'file-saver'
 import Swal from 'sweetalert2';
+import { FaDownload } from 'react-icons/Fa';
+import { AiOutlineWhatsApp } from 'react-icons/Ai';
 
-export const GifItem = ({ title, url }) => {
 
-    const handleDownload = (url) => {
+export const GifItem = ({ title, url, id }) => {
+
+    const handleDownload = (url ) => {
         Swal.fire({
-            title: '¿Segúr@ desea descargar el gif?',
-            icon: 'question',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sí, adelante!'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire(
-                'Descargado!',
-                'El gif ha sido descargado.',
-                'success'
-              )
-              saveAs(url, 'image.gif') // Put your image url here.
+            position: 'top-end',
+            icon: 'success',
+            title: 'Descarga Completada',
+            showConfirmButton: false,
+            timer: 1500
+        })
+        saveAs(url, `${title}.gif`) // Put your image url here.
+    }
+
+    const handleCopyClipboard = () => { 
+        const urlWsp = `https://media.giphy.com/media/${id}/giphy.gif`;
+        navigator.clipboard.writeText(urlWsp);
+
+        Swal.fire({
+            title: '¡Copiado al portapapeles!',
+            text: 'Pega el Link en whatssap y comparte el gif',
+            imageUrl: urlWsp,
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: title,
+            showDenyButton: true,
+            denyButtonText: `Ir a whatssap`
+        }).then((result) => {
+            if (result.isDenied) {
+                window.open(
+                    'https://wa.me',
+                    '_blank' 
+                );
             }
-          })
+        })
+        
     }
 
     return (
@@ -32,8 +49,14 @@ export const GifItem = ({ title, url }) => {
             />
             <div>
                 <p className="text-base">{title}</p>
-                <button className="mx-auto mb-5 animate-bounce" onClick={() => handleDownload(url)}>
+                <button className="mx-auto mb-5 animate-bounce grid grid-cols-2 gap-3" >
                     <FaDownload
+                        color={'red'}
+                        size={20}
+                        onClick={() => handleDownload(url)}
+                    />
+                    <AiOutlineWhatsApp 
+                        onClick={handleCopyClipboard}
                         color={'#3EC70B'}
                         size={20}
                     />
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -5,12 +5,12 @@ import Swal from 'sweetalert2';
 export const getGifs = async (category, setLoading) => {
 
     try {
-        const numRandom = Math.floor((Math.random() * (50-0)) +0)
+        const numRandom = Math.floor((Math.random() * (50-0)) +0);
         const url = `
             https://api.giphy.com/v1/gifs/search?api_key=mepMPXpp1J5vvA6ijPABvglOJUX51p4v&q=${category}&limit=15&rating=r&offset=${numRandom}
         `;
         const resp = await fetch(url);
-        const { data } = await resp.json();
+        const { data } = await resp.json(); 
 
         //si no encuentra resultados, devuelve un mensaje de error
         if (data.length === 0) {
@@ -23,11 +23,15 @@ export const getGifs = async (category, setLoading) => {
             return false;
         }
 
+        console.log("Dtaa: ", data)
+
         const gifs = data.map(item => ({
             id: item.id,
             title: item.title,
             url: item.images.downsized_medium.url
         }));
+
+
 
         return gifs
 
